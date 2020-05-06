3137516a5d50485b4ea3f69aa344a1fe34c4844e
valoeghese <valoeghesemods@gmail.com>
2020-05-06 22:23:30 +1200

    remapping complete. now to hook it into the vanilla 'registries' by overriding the onRemap methods

--- src/main/java/io/github/minecraftcursedlegacy/impl/registry/RegistryRemapper.java
+++ src/main/java/io/github/minecraftcursedlegacy/impl/registry/RegistryRemapper.java
@@ -6,10 +6,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.logging.Logger;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
+import io.github.minecraftcursedlegacy.api.registry.Registry;
 import net.minecraft.util.io.AbstractTag;
 import net.minecraft.util.io.CompoundTag;
 
@@ -22,30 +24,39 @@ public class RegistryRemapper {
 			if (!file.createNewFile()) {
 				CompoundTag data = null;
 
+				// read
 				try (DataInputStream dis = new DataInputStream(new GZIPInputStream(new FileInputStream(file)))) {
 					data = (CompoundTag) AbstractTag.readTag(dis);
 				}
 
-				CompoundTag data0 = data;
+				CompoundTag newData = new CompoundTag();
+				Iterator<Registry<?>> iter = RegistryImpl.registries().iterator();
 
-				RegistryImpl.registries().forEach(registry -> {
+				// remap and add new data
+				while (iter.hasNext()) {
+					Registry<?> registry = iter.next();
 					String key = registry.getRegistryName().toString();
 
-					if (data0.containsKey(key)) {
-						registry.remap(data0.getCompoundTag(key));
+					if (data.containsKey(key)) {
+						newData.put(key, registry.remap(data.getCompoundTag(key)));
+					} else {
+						newData.put(key, registry.toTag());
 					}
-				});
+				}
 
+				// write
 				try (DataOutputStream dos = new DataOutputStream(new GZIPOutputStream(new FileOutputStream(file)))) {
-					
+					AbstractTag.writeTag(newData, dos);
 				}
 			} else {
 				CompoundTag data = new CompoundTag();
 
+				// add data
 				RegistryImpl.registries().forEach(registry -> {
 					data.put(registry.getRegistryName().toString(), registry.toTag());
 				});
 
+				// write
 				try (DataOutputStream dos = new DataOutputStream(new GZIPOutputStream(new FileOutputStream(file)))) {
 					AbstractTag.writeTag(data, dos);
 				}
