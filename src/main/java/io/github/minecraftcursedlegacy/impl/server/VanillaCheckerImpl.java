a4c3f1911522f03c9b73613b9cc3445ff42d900e
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-05-10 21:10:54 -0500

    missed thing

--- src/main/java/io/github/minecraftcursedlegacy/impl/server/VanillaCheckerImpl.java
+++ src/main/java/io/github/minecraftcursedlegacy/impl/server/VanillaCheckerImpl.java
@@ -3,7 +3,7 @@ package io.github.minecraftcursedlegacy.impl.server;
 import java.util.HashMap;
 
 public final class VanillaCheckerImpl {
-	private VanillaCheckerImpl(){
+	private VanillaCheckerImpl() {
 	}
 	public static HashMap<String, Boolean> playermap = new HashMap<String, Boolean>();
 }
