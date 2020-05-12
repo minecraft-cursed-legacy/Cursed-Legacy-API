8317719ff8dc0a416a3cf920b081887a7d880040
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-05-11 21:03:10 -0500

    add networking api

---
+++ src/main/java/io/github/minecraftcursedlegacy/api/networking/PluginChannel.java
@@ -0,0 +1,41 @@
+package io.github.minecraftcursedlegacy.api.networking;
+
+import io.github.minecraftcursedlegacy.impl.networking.PluginMessagePacket;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.ServerPlayer;
+import net.minecraft.network.PacketHandler;
+
+public abstract class PluginChannel {
+	/**
+	 * Returns the unique channel identifier of this channel in the form of modid:channelname
+	 * This should always have the same return value.
+	 * @return Channel Identifier
+	 */
+	public abstract String getChannelIdentifier();
+	
+	/**
+	 * Called when this channel recieves a packet
+	 * @param arg The local PacketHandler
+	 * @param data The data of the packet
+	 */
+	public abstract void onRecieve(PacketHandler arg, byte[] data);
+	
+	/**
+	 * Server packet send method
+	 * @param data The data to send
+	 * @param player The player to send the data to
+	 */
+	public void send(byte[] data, ServerPlayer player) {
+		player.field_255.method_835(new PluginMessagePacket(getChannelIdentifier(), data));
+	}
+
+	/**
+	 * Client packet send method
+	 * @param data The data to send
+	 * @param mc The local Minecraft
+	 */
+	public void send(byte[] data, Minecraft mc) {
+		mc.method_2145().sendPacket(new PluginMessagePacket(getChannelIdentifier(), data));
+	}
+
+}
