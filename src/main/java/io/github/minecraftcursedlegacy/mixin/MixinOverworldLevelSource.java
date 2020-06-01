77ec286f83d9f4f6c79848ee3041b9a08ff553f5
Chocohead <Chocohead@Aroma1997.org>
2020-06-02 00:37:59 +0100

    Update Mixin mapping
    Missed this last time the mappings changed

--- src/main/java/io/github/minecraftcursedlegacy/mixin/MixinOverworldLevelSource.java
+++ src/main/java/io/github/minecraftcursedlegacy/mixin/MixinOverworldLevelSource.java
@@ -18,17 +18,17 @@ public class MixinOverworldLevelSource {
 	@Shadow
 	private Random rand;
 	@Shadow
-	private Level field_2260;
+	private Level level;
 
-	@Inject(at = @At(value = "INVOKE", target = " net/minecraft/level/Level.getBiomeSource()Lnet/minecraft/level/gen/BiomeSource;", ordinal = 1), method = "decorate")
+	@Inject(at = @At(value = "INVOKE", target = "net/minecraft/level/Level.getBiomeSource()Lnet/minecraft/level/gen/BiomeSource;", ordinal = 1), method = "decorate")
 	private void onDecorate(LevelSource levelSource, int x, int z, CallbackInfo info) {
 		// the parameter variables x and z are multiplied by 16 in the code early on
 		// but for some reason it doesn't seem to affect this
 		x *= 16;
 		z *= 16;
 		ChunkDecorateCallback.EVENT.invoker().onDecorate(
-				this.field_2260,
-				this.field_2260.getBiomeSource().getBiome(x + 16, z + 16),
+				this.level,
+				this.level.getBiomeSource().getBiome(x + 16, z + 16),
 				this.rand,
 				x,
 				z);
