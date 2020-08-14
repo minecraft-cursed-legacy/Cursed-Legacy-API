326a91aac9adbf7bf4ec0755d541306dfbd26207
valoeghese <46117728+valoeghese@users.noreply.github.com>
2020-08-15 01:44:49 +1200

    Delete MixinItemEntity.java

--- src/main/java/io/github/minecraftcursedlegacy/mixin/MixinItemEntity.java
+++
@@ -1,9 +0,0 @@
-package io.github.minecraftcursedlegacy.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-import net.minecraft.entity.ItemEntity;
-
-@Mixin(ItemEntity.class)
-public class MixinItemEntity {
-}
