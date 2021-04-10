d35020cef76e7bf0e28adff11b52da39be0e7e6b
William Bradford Larcombe <william@williambl.com>
2021-04-10 12:20:23 +0100

    Remap the other tile arrays when tile registry is remapped (#27)
    * Remap the other tile arrays when tile registry is remapped
    
    * Update RegistryImpl.java
    
    Co-authored-by: Mekal Covic <46117728+valoeghese@users.noreply.github.com>

--- src/main/java/io/github/minecraftcursedlegacy/impl/registry/RegistryImpl.java
+++ src/main/java/io/github/minecraftcursedlegacy/impl/registry/RegistryImpl.java
@@ -37,6 +37,14 @@ public class RegistryImpl implements ModInitializer {
 	}
 
 	private static class TileRegistry extends Registry<Tile> {
+		private final Map<Tile, Boolean> ticksRandomly = new HashMap<>();
+		private final Map<Tile, Boolean> isFullOpaque = new HashMap<>();
+		private final Map<Tile, Boolean> hasTileEntity = new HashMap<>();
+		private final Map<Tile, Integer> field_1941 = new HashMap<>();
+		private final Map<Tile, Boolean> field_1942 = new HashMap<>();
+		private final Map<Tile, Integer> field_1943 = new HashMap<>();
+		private final Map<Tile, Boolean> field_1944 = new HashMap<>();
+
 		private TileRegistry(Id registryName) {
 			super(Tile.class, registryName, null);
 
@@ -77,7 +85,29 @@ public class RegistryImpl implements ModInitializer {
 		@Override
 		protected void beforeRemap() {
 			int size = Tile.BY_ID.length;
+
+			for (int i = 1; i < size; i++) { // Starting at 1 as microoptimisation because 0 is taken by a forced null value: Air
+				Tile tile = Tile.BY_ID[i];
+				
+				if (tile != null) {
+					ticksRandomly.put(tile, Tile.TICKS_RANDOMLY[i]);
+					isFullOpaque.put(tile, Tile.FULL_OPAQUE[i]);
+					hasTileEntity.put(tile, Tile.HAS_TILE_ENTITY[i]);
+					field_1941.put(tile, Tile.field_1941[i]);
+					field_1942.put(tile, Tile.field_1942[i]);
+					field_1943.put(tile, Tile.field_1943[i]);
+					field_1944.put(tile, Tile.field_1944[i]);
+				}
+			}
+
 			System.arraycopy(new Tile[size], 0, Tile.BY_ID, 0, size);
+			System.arraycopy(new boolean[size], 0, Tile.TICKS_RANDOMLY, 0, size);
+			System.arraycopy(new boolean[size], 0, Tile.FULL_OPAQUE, 0, size);
+			System.arraycopy(new boolean[size], 0, Tile.HAS_TILE_ENTITY, 0, size);
+			System.arraycopy(new int[size], 0, Tile.field_1941, 0, size);
+			System.arraycopy(new boolean[size], 0, Tile.field_1942, 0, size);
+			System.arraycopy(new int[size], 0, Tile.field_1943, 0, size);
+			System.arraycopy(new boolean[size], 0, Tile.field_1944, 0, size);
 		}
 
 		@Override
@@ -91,6 +121,14 @@ public class RegistryImpl implements ModInitializer {
 			if (tileItem != null) {
 				((ParentIdSetter) tileItem).setParentId(newSerialisedId);
 			}
+
+			Tile.TICKS_RANDOMLY[newSerialisedId] = ticksRandomly.getOrDefault(remappedValue, false);
+			Tile.FULL_OPAQUE[newSerialisedId] = isFullOpaque.getOrDefault(remappedValue, false);
+			Tile.HAS_TILE_ENTITY[newSerialisedId] = hasTileEntity.getOrDefault(remappedValue, false);
+			Tile.field_1941[newSerialisedId] = field_1941.getOrDefault(remappedValue, 0);
+			Tile.field_1942[newSerialisedId] = field_1942.getOrDefault(remappedValue, false);
+			Tile.field_1943[newSerialisedId] = field_1943.getOrDefault(remappedValue, 0);
+			Tile.field_1944[newSerialisedId] = field_1944.getOrDefault(remappedValue, false);
 		}
 	}
 
